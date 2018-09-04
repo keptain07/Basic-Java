f8283942b8d38be98aa74b2e378c3dd55ed15647
keptain07 <42082064+keptain07@users.noreply.github.com>
2018-09-04 03:06:32 -0500

    Add files via upload

---
+++ Variables/src/Arrays.java
@@ -0,0 +1,14 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int a[] = { 1, 2, 3, 4, 5 };
+		int i = 0;
+		while (i < 3) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
